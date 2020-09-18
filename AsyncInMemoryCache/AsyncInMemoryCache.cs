8110a8c3048fd40cd67eedd7e292287eecc974b2
marek <horacek@argutec.eu>
2020-09-18 12:43:31 +0200

    Change AsyncInMemoryCache namespace

--- AsyncInMemoryCache/AsyncInMemoryCache.cs
+++ AsyncInMemoryCache/AsyncInMemoryCache.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace AsyncInMemoryCache
+namespace Argutec.AsyncInMemoryCache
 {
     public class AsyncInMemoryCache
     {
